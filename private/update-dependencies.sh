3b10228ade238b0abde97a46f03a81cca6edda00
ChrisBAshton <chrisashtonweb@gmail.com>
2015-03-30 23:15:29 +0100

    added subdomain config

--- private/update-dependencies.sh
+++ private/update-dependencies.sh
@@ -10,4 +10,17 @@ php composer.phar install
 # make dependencies bin available
 export PATH=./vendor/bin:$PATH
 
+sudo -i
+sudo echo '<VirtualHost *:80>
+    DocumentRoot /var/www/html
+    ServerName  www.smartresolution.org
+    ServerAlias smartresolution.org
+</VirtualHost>
+
+<VirtualHost *:80>
+   ServerName   demo.smartresolution.org
+   DocumentRoot /var/www/html-demo
+</VirtualHost>' > /etc/httpd/conf.d/subdomains.conf
+exit
+
 cd -

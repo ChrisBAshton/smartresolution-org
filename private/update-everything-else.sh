fb234b1243afe39e0ce252263df7f4463dcb510a
ChrisBAshton <chrisashtonweb@gmail.com>
2015-03-31 13:55:57 +0100

    fix script calls

--- private/update-everything-else.sh
+++ private/update-everything-else.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# can call this script from anywhere, we're correcting the path dynamically
-my_dir="$(dirname "$0")"
-"sudo $my_dir/update-zip-and-documentation.sh"
-"sudo $my_dir/update-demo-version.sh"
-"sudo $my_dir/update-dependencies.sh"
+source /var/www/html/private/update-zip-and-documentation.sh
+source /var/www/html/private/update-demo-version.sh
+source /var/www/html/private/update-dependencies.sh
